42f0fe6f9494977e09b8f553a32746a87bf702e8
Abel <addis0132@gmail.com>
2024-12-01 11:07:38 +0300

    v3 of django-models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LibraryDetailView
+from .views import list_books, LibraryDetailView
 from . import views
 from django.contrib.auth.views import LoginView,LogoutView
 urlpatterns = [
